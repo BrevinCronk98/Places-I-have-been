77470c5aef0919580fa08d97acf5182dada310a4
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-04-06 14:57:07 -0700

    functionality works as expected. user can see all entries on html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function EntryList()
 {
-    this.entries = [];
-    this.currentId = 0;
+    this.entries = [],
+    this.currentId = 0
 }
 
 EntryList.prototype.addEntry = function(entry){
@@ -49,36 +49,36 @@ function Places(locations, landmarks, time, notes) {
 
   // UI --------
   var entryList = new EntryList();
-  
-//   function displayEntryDetail(entryBookToDisplay){
-//       var travelList = $("ul#list");
-//       var htmlForTravelInfo = "";
-//       entryBookToDisplay.entries.forEach(function(entry) {
-//           htmlForTravelInfo += "<li id="  entry.id + ">" entry.locationsInput + " " + entry.landmarkInput + "</li>";      
-//   });
-//   travelList.html(htmlForTravelInfo);
-// };
-
+   
+  function displayEntryInfo(entryBookToDisplay) {
+      var travelList = $("ul#list");
+      var htmlForInfo = "";
+      entryBookToDisplay.entries.forEach(function(entry) {
+            htmlForInfo += "<li id=>" + entry.id + ": " + "<br>" + "Country: " + entry.locations +  ", " + "<br>" + "Landmarks: " + entry.landmarks + ", "+ "<br>" + "Date: " + entry.time + ", " + "<br>" + "Notes: " + entry.notes + "</li>";
+      });
+      travelList.html(htmlForInfo);
+  }
 
 $(document).ready(function() {
   $("form#place-info").submit(function(event){
     event.preventDefault();
-    var locationsInput = $("input#country").val();
-    var landmarkInput = $("input#landmark").val();
-    var timeInput = $("input#date").val();
-    var notesInput = $("input#notes").val();
-    var newPlaces = new Places (locationsInput, landmarkInput, timeInput, notesInput);
+    var locationsInput = $("input#country");
+    var landmarkInput = $("input#landmark");
+    var timeInput = $("input#date");
+    var notesInput = $("input#notes");
+    var newPlaces = new Places (locationsInput.val(), landmarkInput.val(), timeInput.val(), notesInput.val());
     entryList.addEntry(newPlaces);
     // console.log(newPlaces);
-    console.log(entryList.entries)
-    //displayEntryDetail(entryList);
+    console.log(entryList.entries);
+    console.log(entryList.landmark);
+    displayEntryInfo(entryList);
     
-    $("#display-travels").text(entryList.entries[0]);
+    // $("#display-travels").text(entryList.entries);
     
-    $("#display-locations").text(newPlaces.locations);
-    $("#display-landmark").text(newPlaces.landmarks).hide();
-    $("#display-date").text(newPlaces.time).hide();
-    $("#display-notes").text(newPlaces.notes).hide();
+    // $("#display-locations").text(newPlaces.locations);
+    // $("#display-landmark").text(newPlaces.landmarks).hide();
+    // $("#display-date").text(newPlaces.time).hide();
+    // $("#display-notes").text(newPlaces.notes).hide();
 
     // $("#display-locations").click(function() {
     //   $("#display-landmark").show();
