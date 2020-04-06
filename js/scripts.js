395d7dfb18c834a94bd61b25ad908ee78affef87
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-04-06 13:44:00 -0700

    the app stores old results in new variable and display all previos results on console

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,93 @@
+function EntryList()
+{
+    this.entries = [];
+    this.currentId = 0;
+}
+
+EntryList.prototype.addEntry = function(entry){
+    entry.id = this.assignId();
+    this.entries.push(entry);
+}
+
+EntryList.prototype.assignId = function(){
+    this.currentId += 1;
+    return this.currentId;
+}
+
+EntryList.prototype.findEntry = function(id) {
+    for (var i=0; i< this.entries.lenth; i++){
+        if(this.entries[i]){
+            if(this.entries[i].id == id){
+                return this.entries[i]
+            }
+        }
+    };
+    return false;
+}
+
+EntryList.prototype.deleteEntry = function(id) {
+    for (var i=0; i< this.this.entries.length; i++){
+        if(this.entries[i]){
+            if(this.entries[i].id == id){
+                delete this.entries[i];
+                return true;
+            } 
+        }
+    };
+    return false;
+}
+
+
 function Places(locations, landmarks, time, notes) {
     this.locations = locations;
     this.landmarks = landmarks;
     this.time = time;
-    this.notes= notes;
-}
-// function showEntries(place) {
-//     var display = $("#test");
-//     var entryDetails = "";
-//     entryDetails += `<h2> ${place.locations}</h2>`;
-//     entryDetails += `<p> ${place.landmarks}</p>`;
-//     entryDetaails += `<p> ${place.time}</p>`;
-//     entryDetails += `<p> ${ place.notes}</p>`;
-//     display.html(entryDetails)
-// }
+    this.notes = notes;
+  }
+
+
+
+  // UI --------
+  var entryList = new EntryList();
+  
+//   function displayEntryDetail(entryBookToDisplay){
+//       var travelList = $("ul#list");
+//       var htmlForTravelInfo = "";
+//       entryBookToDisplay.entries.forEach(function(entry) {
+//           htmlForTravelInfo += "<li id="  entry.id + ">" entry.locationsInput + " " + entry.landmarkInput + "</li>";      
+//   });
+//   travelList.html(htmlForTravelInfo);
+// };
+
 
 $(document).ready(function() {
   $("form#place-info").submit(function(event){
     event.preventDefault();
-    var locationsInput = $("#country")
-    var landmarkInput = $("#landmark")
-    var timeInput = $("#date")
-    var notesInput = $("#notes")
-    //var inputArr = [locationsInput, landmarkInput, timeInput, notesInput];
-    //console.log(inputArr);
-    var newPlaces = new Places (locationsInput.val(), landmarkInput.val(), timeInput.val(), notesInput.val());
-    console.log(newPlaces);
-    // showEntries();
+    var locationsInput = $("input#country").val();
+    var landmarkInput = $("input#landmark").val();
+    var timeInput = $("input#date").val();
+    var notesInput = $("input#notes").val();
+    var newPlaces = new Places (locationsInput, landmarkInput, timeInput, notesInput);
+    entryList.addEntry(newPlaces);
+    // console.log(newPlaces);
+    console.log(entryList.entries)
+    //displayEntryDetail(entryList);
+    
+    $("#display-travels").text(entryList.entries[0]);
     
     $("#display-locations").text(newPlaces.locations);
-    $("#display-landmark").text(newPlaces.landmarks);
-    $("#display-date").text(newPlaces.time);
-    $("#display-notes").text(newPlaces.notes);
+    $("#display-landmark").text(newPlaces.landmarks).hide();
+    $("#display-date").text(newPlaces.time).hide();
+    $("#display-notes").text(newPlaces.notes).hide();
+
+    // $("#display-locations").click(function() {
+    //   $("#display-landmark").show();
+    //   $("#display-date").show();
+    //   $("#display-notes").show();
+      
+    // });
+
+  
     
     //alert(newPlaces)
   });
